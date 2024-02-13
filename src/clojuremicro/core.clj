1c64bd2a8236b4a5d09fd0032d8b5242efa03cea
coderHuahua <86610939+coderHuahua@users.noreply.github.com>
2024-02-13 12:11:25 -0500

    Initial commit

---
+++ src/clojuremicro/core.clj
@@ -0,0 +1,11 @@
+(ns clojuremicro.core)
+
+
+
+
+;; define the function nor-convert
+;;A sample REPL interaction with your program might look like the following
+;;a2=> (def e1 '(and x y true))
+;;#'user/e1
+;;a2=> (nor-convert e1)
+;;(nor (nor x) (nor y) (nor true))

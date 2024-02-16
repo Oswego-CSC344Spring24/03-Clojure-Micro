721883cf9a41f06a9c543985e3b59db3c0486769
lromano2oswego <157433850+lromano2oswego@users.noreply.github.com>
2024-02-15 20:57:40 -0500

    Update core.clj

--- src/clojuremicro/core.clj
+++ src/clojuremicro/core.clj
@@ -1,11 +1,25 @@
 (ns clojuremicro.core)
 
+(defn not-expr [expr]
+  "Converts a not-expression to a nor-expression."
+  (let [[_ x] expr]                                         ;get the variable x and ignore not
+    (list 'nor x x)))                                       ;return nor-expression created from input
 
+(defn and-expr [expr]
+  "Converts an and-expression to a nor-expression"
+  (let [[_ & args]expr]                                     ;get all arguments while ignoring and
+    (cons 'nor (map (fn [arg] (list 'nor arg arg)) args)))) ;uses map to convert each argument
 
+(defn or-expr [expr]
+  "Converts an or-expression to a nor-expression"
+  (let [[_ & args] expr]                                    ;get all arguments and ignore or
+    (list 'nor (cons 'nor args) (cons 'nor args))))         ;double negated arguments
 
-;; define the function nor-convert
-;;A sample REPL interaction with your program might look like the following
-;;a2=> (def e1 '(and x y true))
-;;#'user/e1
-;;a2=> (nor-convert e1)
-;;(nor (nor x) (nor y) (nor true))
+(defn nor-convert [expr]
+  "Converts any logical-expression to a nor-expression"
+  cond (
+        (= (first expr) 'not) (not-expr expr)
+        (= (first expr) 'and) (and-expr expr)
+        (= (first expr) 'or) (or-expr expr)
+        :else (throw (IllegalArgumentException. "Expression not found"))))
+

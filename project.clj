57550b237dbe7af448288c6f5d2233959c71fd84
tglynn2 <158096838+tglynn2@users.noreply.github.com>
2024-02-18 20:29:46 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
-(defproject clojuremicro "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns clojuremicro.core})
+(defn norConvert [expression]
+  (cond
+    (= 'not (first expression))
+    (apply list 'nor (rest expression))
+
+    (= 'and (first expression))
+    (apply list 'nor (map #(list 'nor %) (rest expression)))
+
+    (= 'or (first expression))
+    (list 'nor (apply list 'nor (rest expression)))
+
+    :else expression)
+  )

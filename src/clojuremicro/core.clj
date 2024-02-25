65a1824ecacfa85cfcc0cf905b4f5e3205df2162
euwiduha <euwiduha@oswego.edu>
2024-02-25 12:27:28 -0500

    ready

--- src/clojuremicro/core.clj
+++ src/clojuremicro/core.clj
@@ -1,13 +1,12 @@
 (ns user)
-; Define the nor-convert function
+
 (defn nor-convert [expr]
   "Converts an expression to a NOR-based expression."
   (let [op (first expr)
         args (rest expr)]
     (cond
-      (= op 'not) ['nor (first args)]  ; Convert NOT directly to NOR
-      (= op 'or)  (cons 'nor args)     ; Convert OR to NOR by just changing the operator
-      (= op 'and) ; Convert AND to a sequence of NORs
+      (= op 'not) ['nor (first args)] 
+      (= op 'or)  (cons 'nor args)    
+      (= op 'and) ; 
       (cons 'nor (map #(list 'nor %) args))
-      :else expr))) ; Return the expression unchanged if it does not start with not, and, or or
-
+      :else expr))) 

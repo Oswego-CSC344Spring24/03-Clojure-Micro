e880c67e247792eb7ae70a28e615eced49c443a5
euwiduha <157419300+euwiduha@users.noreply.github.com>
2024-02-26 18:56:18 -0500

    Update core.clj

--- src/clojuremicro/core.clj
+++ src/clojuremicro/core.clj
@@ -1,13 +1,23 @@
 (ns user)
 
-(defn nor-convert [expr]
-  "Converts an expression to a NOR-based expression ."
-  (let [op (first expr)
-        args (rest expr)]
-    (cond
-      (= op 'not) ['nor (first args)] 
-      (= op 'or)  (cons 'nor args)    
-      (= op 'and) ; 
-      (cons 'nor (map #(list 'nor %) args))
-      :else expr))) 
-;Crazy? I was crazy once. They locked me in a room. A rubber room. A rubbeer room full of rats. And rats. rats make me crazy.
+(defn not-to-nor [expr]
+  (if (= (first expr) 'not)
+    ['nor (second expr)]
+    (throw (IllegalArgumentException. "Expected a not expression"))))
+
+(defn or-to-nor [expr]
+  (if (= (first expr) 'or)
+    (cons 'nor (rest expr))
+    (throw (IllegalArgumentException. "Expected an or expression"))))
+
+(defn and-to-nor [expr]
+  (if (= (first expr) 'and)
+    (cons 'nor (map #(list 'nor %) (rest expr)))
+    (throw (IllegalArgumentException. "Expected an and expression"))))
+
+(defn convert-expr [expr]
+  (cond
+    (= (first expr) 'not) (not-to-nor expr)
+    (= (first expr) 'or)  (or-to-nor expr)
+    (= (first expr) 'and) (and-to-nor expr)
+    else (throw (IllegalArgumentException. "Unknown expression type"))))

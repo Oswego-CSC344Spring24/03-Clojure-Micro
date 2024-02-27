52993621d0b6430d2bc8315eee73af56148d08f0
euwiduha <157419300+euwiduha@users.noreply.github.com>
2024-02-26 19:10:46 -0500

    Update core.clj

--- src/clojuremicro/core.clj
+++ src/clojuremicro/core.clj
@@ -2,12 +2,13 @@
 
 (defn not-to-nor [expr]
   (if (= (first expr) 'not)
-    [list 'nor (second expr)]
+    (list 'nor (second expr)) 
     (throw (IllegalArgumentException. "Expected a not expression"))))
 
 (defn or-to-nor [expr]
+  
   (if (= (first expr) 'or)
-    (cons 'nor (cons (rest expr) nil))
+    (cons 'nor (rest expr)) 
     (throw (IllegalArgumentException. "Expected an or expression"))))
 
 (defn and-to-nor [expr]
@@ -15,9 +16,10 @@
     (cons 'nor (map #(list 'nor %) (rest expr)))
     (throw (IllegalArgumentException. "Expected an and expression"))))
 
-(defn convert-expr [expr]
+(defn nor-convert [expr]
   (cond
     (= (first expr) 'not) (not-to-nor expr)
     (= (first expr) 'or)  (or-to-nor expr)
     (= (first expr) 'and) (and-to-nor expr)
-    else (throw (IllegalArgumentException. "Unknown expression type"))))
+    :else (throw (IllegalArgumentException. "Unknown expression type"))))
+

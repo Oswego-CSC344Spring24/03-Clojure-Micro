1d87964c7acc0c03be08fde324c09a24480dd3df
euwiduha <157419300+euwiduha@users.noreply.github.com>
2024-02-26 19:17:21 -0500

    Update core.clj

--- src/clojuremicro/core.clj
+++ src/clojuremicro/core.clj
@@ -6,9 +6,8 @@
     (throw (IllegalArgumentException. "Expected a not expression"))))
 
 (defn or-to-nor [expr]
-  
   (if (= (first expr) 'or)
-    (cons 'nor (rest expr)) 
+    (list 'nor (conj (list 'nor) (rest expr))) ; Modified to nest correctly
     (throw (IllegalArgumentException. "Expected an or expression"))))
 
 (defn and-to-nor [expr]
@@ -22,4 +21,5 @@
     (= (first expr) 'or)  (or-to-nor expr)
     (= (first expr) 'and) (and-to-nor expr)
     :else (throw (IllegalArgumentException. "Unknown expression type"))))
+
 

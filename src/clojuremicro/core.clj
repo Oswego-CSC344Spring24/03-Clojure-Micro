b6161ae808de05770f8e2052197c76dc468384b0
tglynn2 <158096838+tglynn2@users.noreply.github.com>
2024-02-26 20:08:45 -0500

    Update core.clj

--- src/clojuremicro/core.clj
+++ src/clojuremicro/core.clj
@@ -1,5 +1,17 @@
 (ns clojuremicro.core)
+(defn norConvert [expression]
+  (cond
+    (= 'not (first expression))
+    (apply list 'nor (rest expression))
 
+    (= 'and (first expression))
+    (apply list 'nor (map #(list 'nor %) (rest expression)))
+
+    (= 'or (first expression))
+    (list 'nor (apply list 'nor (rest expression)))
+
+    :else expression)
+  )
 
 
 
